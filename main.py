a48e5bbc0675d2b69b5e2d7a27bf2dc6ef736d18
Denis Berretti <denis.berretti95@gmail.com>
2025-06-28 15:59:25 +0000

    Implementato devcontainer python base

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Prints 'Hello, world!' to the console."""
+
+
+def hello_world():
+    """Prints 'Hello, world!' to the console."""
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    hello_world()
